d1255c1894d5b02d5a738ac89f1bfae3e6f19fa6
Juan sanchez <adm-master-90@hotmail.com>
2020-06-11 20:38:45 -0400

    change keys

--- src/components/PaymentCalculator/Payment.ts
+++ src/components/PaymentCalculator/Payment.ts
@@ -61,7 +61,7 @@ export default class PaymentCalculator extends (FieldComponent as any) {
                                 {
                                     label: 'Monthly Payment',
                                     currency: 'USD',
-                                    calculateValue: "function AM(principal, rate, period, yearOrMonth, payAtBeginning) {\n  var numerator, denominator, am;\n  var ratePerPeriod = rate / 12 / 100;\n\n  // for inputs in years\n  if (!yearOrMonth) {\n    numerator = buildNumerator(period * 12);\n    denominator = Math.pow((1 + ratePerPeriod), period * 12) - 1;\n\n    // for inputs in months\n  } else if (yearOrMonth === 1) {\n    numerator = buildNumerator(period)\n    denominator = Math.pow((1 + ratePerPeriod), period) - 1;\n\n  } else {\n    console.log('not defined');\n  }\n  am = principal * (numerator / denominator);\n  return Math.round(am * 100) / 100;\n\n  function buildNumerator(numInterestAccruals) {\n    if (payAtBeginning) {\n      //if payments are made in the beginning of the period, then interest shouldn't be calculated for first period\n      numInterestAccruals -= 1;\n    }\n    return ratePerPeriod * Math.pow((1 + ratePerPeriod), numInterestAccruals);\n  }\n};\nvar rate = row.rate;\nvar term = row.term;\nvar principle = row.loanAmount;\n\ndebugger;value = AM(principle, rate, term, months = 1, false);",
+                                    calculateValue: "function AM(principal, rate, period, yearOrMonth, payAtBeginning) {\n  var numerator, denominator, am;\n  var ratePerPeriod = rate / 12 / 100;\n\n  // for inputs in years\n  if (!yearOrMonth) {\n    numerator = buildNumerator(period * 12);\n    denominator = Math.pow((1 + ratePerPeriod), period * 12) - 1;\n\n    // for inputs in months\n  } else if (yearOrMonth === 1) {\n    numerator = buildNumerator(period)\n    denominator = Math.pow((1 + ratePerPeriod), period) - 1;\n\n  } else {\n    console.log('not defined');\n  }\n  am = principal * (numerator / denominator);\n  return Math.round(am * 100) / 100;\n\n  function buildNumerator(numInterestAccruals) {\n    if (payAtBeginning) {\n      //if payments are made in the beginning of the period, then interest shouldn't be calculated for first period\n      numInterestAccruals -= 1;\n    }\n    return ratePerPeriod * Math.pow((1 + ratePerPeriod), numInterestAccruals);\n  }\n};\nvar rate = row.rate1;\nvar term = row.term1;\nvar principle = row.loanAmount1;\n\ndebugger;value = AM(principle, rate, term, months = 1, false);",
                                     key: 'monthlyPayment',
                                     type: 'currency',
                                     input: true,
@@ -76,7 +76,7 @@ export default class PaymentCalculator extends (FieldComponent as any) {
         });
     }
     static builderInfo = {
-        title: 'paymentCalculator',
+        title: 'Payment Calculator',
         group: 'basic',
         icon: 'fa fa-table',
         weight: 70,
